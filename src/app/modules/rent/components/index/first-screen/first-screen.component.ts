76b5a5a27f74ba078d9a38c09a0aada2743d6cdf
Alexandr <XoxlofAlexandr@yandex.ru>
2020-02-11 00:54:43 +0300

    добавлены отступы для блоков апартаментов на странице аренды, добавлен вызов формы фидбэка на первом экране страницы аренды

--- src/app/modules/rent/components/index/first-screen/first-screen.component.ts
+++ src/app/modules/rent/components/index/first-screen/first-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/shared/services/modal.service';
 
 @Component({
   selector: 'app-first-screen',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstScreenComponent implements OnInit {
 
-  constructor() { }
+  constructor(public modal: ModalService) { }
 
   ngOnInit() {
   }
