58db99cb25838afadca33313749f511719786841
Alexandr <XoxlofAlexandr@yandex.ru>
2020-02-21 00:38:30 +0300

    добавлен модуль click-outside, добавлена логика закрытия раскрывающегося меню при клике вне его компонента

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuService } from './shared/services/menu.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vhc';
+
+  constructor(public menu: MenuService) {
+    setInterval(() => {
+      console.log(this.menu.showMenu);
+    }, 1000);
+  }
 }
