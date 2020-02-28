601a00c3aa5010f9ba5e18f9e4f74409b80038b8
Alexandr <XoxlofAlexandr@yandex.ru>
2020-02-29 01:55:33 +0300

    добавлена задержка для отображения страниц при первичном заходе на сайт

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuService } from './shared/services/menu.service';
 
 @Component({
@@ -6,8 +6,16 @@ import { MenuService } from './shared/services/menu.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'vhc';
+  routingEnabled: boolean = false;
 
   constructor(public menu: MenuService) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.routingEnabled = true;
+    }, 650);
+  }
 }
