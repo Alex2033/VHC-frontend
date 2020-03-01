1438e4272dd4329eaa9db7f87c424b671f519d62
Alexandr <XoxlofAlexandr@yandex.ru>
2020-03-01 07:21:15 +0300

    добавлена анимация лоадинга при первом заходе на сайт

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuService } from './shared/services/menu.service';
 import * as AOS from 'aos';
+import { LoadingAnimationService } from './shared/services/loading-animation.service';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +12,27 @@ export class AppComponent implements OnInit {
   
   title = 'vhc';
   routingEnabled: boolean = false;
+  showWhiteBlock: boolean = true;
+  showTitle: boolean = true;
 
-  constructor(public menu: MenuService) {}
+  constructor(public menu: MenuService, public animationLoading: LoadingAnimationService) {}
 
   ngOnInit() {
     setTimeout(() => {
+      this.animationLoading.animationLoading = false;
+    }, 2350);
+
+    setTimeout(() => {
+      this.showWhiteBlock = false;
+    }, 1750);
+
+    setTimeout(() => {
+      this.showTitle = false;
+    }, 2000);
+    
+    setTimeout(() => {
       this.routingEnabled = true;
-    }, 650);
+    }, 2350);
 
     AOS.init({
       once: true
